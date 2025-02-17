12da5f45634d7a689bafd5065e5fc46f0f91ddfe
MinhDang1701 <minh82320@gmail.com>
2025-02-17 07:19:21 +0700

    bai2

--- Processor.java
+++ Processor.java
@@ -2,17 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Bai1Student;
+package Bai2animal;
 
-import java.util.Scanner;
-
+/**
+ *
+ * @author ThinkPad
+ */
 public class Processor {
-   
-
-
     public static void main(String[] args) {
-        StudentList studentList = new StudentList();
-        studentList.processStudents();
+        AnimalList animalList = new AnimalList();
+
+        Animal dog1 = new Animal("111", "Vietnam", "1212", "fdsdsd", "Dog");
+        Animal dog2 = new Animal("222", "USA", "1313", "red", "Dog");
+
+        Animal cat1 = new Animal("333", "UK", "4343", "White", "Cat");
+        Animal cat2 = new Animal("444", "Japan", "5454", "Gray", "Cat");
+
+        animalList.addAnimal(dog1);
+        animalList.addAnimal(dog2);
+        animalList.addAnimal(cat1);
+        animalList.addAnimal(cat2);
+
+        animalList.displayAll();
     }
 }
+
 
